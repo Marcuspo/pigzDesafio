5f8944be5321720474236ed44175bbc0c9ce1e48
Marcus Pontes <marcus.pontes@a2wplataforma.com.br>
2023-06-12 15:27:33 -0300

    Install dependecies and create login page

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/Login/index';
+import { enableFreeze } from 'react-native-screens';
+import { StatusBar } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+enableFreeze(true);
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        {/* <StatusBar barStyle="light-content" /> // ajustar por padrao do dispositivo */} 
+        <Stack.Navigator >
+          <Stack.Screen name='Login' component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+const Login = () => {
+     return (
+          <View>
+               <Text>Teste</Text>
+          </View>
+     )
+}
+
+export default Login;
